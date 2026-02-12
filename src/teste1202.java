3fff0e06ad869ca61d80e18a843df68dbcda7fc7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-12 11:05:39 -0300

    enviando teste_git_02

--- src/teste1202.java
+++ src/teste1202.java
@@ -1,6 +1,6 @@
 public class teste1202 {
     public static void main(String[] args) {
-
+        System.out.println("teste de gitgit");
 
 
 
---
+++ src/teste_git_02.java
@@ -0,0 +1,7 @@
+public class teste_git_02 {
+    public static void main(String[] args) {
+        System.out.println("ola mundo!");
+
+
+    }
+}
